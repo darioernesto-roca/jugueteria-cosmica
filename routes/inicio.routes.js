d26d8cb4d6029970c6cf768d3885b46dbd2d50ea
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-06-15 20:25:30 -0500

    Fixing inicio route for getting products from MongoDB and also updating moongose lastest version

--- routes/inicio.routes.js
+++ routes/inicio.routes.js
@@ -1,32 +1,35 @@
 // routes/inicio.routes.js
 const Product = require('../models/product.model');
 
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) {
     const path = req.path;
 
     if (path === '/' || path === '/index.html') {
         console.log("Intentando traer los productos de la base de datos...");
-        Product.find( {}, (err, products) => {
-            if (err) {
-                console.error(err);
-                res.send('Error when getting the products');
-            } else {
-                const newProducts = products.map(product => ({
-                    name: product.name,
-                    price: product.price,
-                    stock: product.stock,
-                    brand: product.brand,
-                    category: product.category,
-                    descriptionShort: product.descriptionShort,
-                    descriptionLong: product.descriptionLong,
-                    freeShipping: product.freeShipping,
-                    ageSince: product.ageSince,
-                    ageUntil: product.ageUntil,
-                    image: product.image,
-                }));
-                res.render('inicio', { products: newProducts, title: 'Juguetería Cósmica | Inicio'});
-            }
-        });
+        try {
+            const products = await Product.find({}).exec();
+            const newProducts = products.map(product => ({
+                name: product.name,
+                price: product.price,
+                stock: product.stock,
+                brand: product.brand,
+                category: product.category,
+                descriptionShort: product.descriptionShort,
+                descriptionLong: product.descriptionLong,
+                freeShipping: product.freeShipping,
+                ageSince: product.ageSince,
+                ageUntil: product.ageUntil,
+                image: product.image,
+            }));
+
+            res.render('inicio', { products: newProducts, title: 'Juguetería Cósmica | Inicio'});
+
+            
+        } catch (err) {
+            console.error(err);
+            res.send('Error when getting the products');
+        }
+
     } else {
         next();
     }
